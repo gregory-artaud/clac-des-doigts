460bb6389ba4c68abdd8f212c6aaae22421365f2
Gregory ARTAUD <gregory.rtd@gmail.com>
2023-02-01 16:48:48 +0100

    chicken methods

--- src/chicken.controller.ts
+++ src/chicken.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 import { ChickenService } from './chicken.service';
 
 @Controller('chicken')
@@ -6,7 +6,27 @@ export class ChickenController {
   constructor(private readonly chickenService: ChickenService) {}
 
   @Get()
-  getHello(): string {
+  getChicken(): string {
+    return this.chickenService.getHello();
+  }
+
+  @Post()
+  postChicken(): string {
+    return this.chickenService.getHello();
+  }
+
+  @Put()
+  putChicken(): string {
+    return this.chickenService.getHello();
+  }
+
+  @Patch()
+  patchChicken(): string {
+    return this.chickenService.getHello();
+  }
+
+  @Delete()
+  deleteChicken(): string {
     return this.chickenService.getHello();
   }
 }
